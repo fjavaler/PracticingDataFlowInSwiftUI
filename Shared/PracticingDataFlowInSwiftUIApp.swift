c51f5ac5643bee4079bdc897423ae476b761ecb8
Frederick Javalera <frederickjavalera@gmail.com>
2021-03-27 19:56:42 -0600

    Initial Commit

---
+++ Shared/PracticingDataFlowInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PracticingDataFlowInSwiftUIApp.swift
+//  Shared
+//
+//  Created by Fred Javalera on 3/27/21.
+//
+
+import SwiftUI
+
+@main
+struct PracticingDataFlowInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
